8673b0c5d8a905dcf91e99dde62d7bf2e42c835f
tomoki <benzousan7@gmail.com>
2022-12-02 17:37:19 +0900

    ブログのバリデーションを追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
+  validates :content,presence: true,length:{in:1..140}
+  validates :title,presence: true,length:{maximum: 20}
   belongs_to :user
   mount_uploader :image, ImageUploader
 end
