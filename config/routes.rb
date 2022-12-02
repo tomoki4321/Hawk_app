2fab6acf0bfaff9a401203f66ee3a2b76dbc1b68
tomoki <benzousan7@gmail.com>
2022-12-02 15:07:00 +0900

    一覧表示を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions, only: [:new,:create,:destroy]
   resources :users, only: [:new,:create,:show]
 end
