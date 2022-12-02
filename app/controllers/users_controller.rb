25fbbbd1c270e4d0b23d3d027665046f679de08e
tomoki <benzousan7@gmail.com>
2022-12-02 17:16:46 +0900

    ユーザー作成後のリダイレクトを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :login_required, only: [:new, :create]
+  skip_before_action :login_required, only: [:new, :create,:show]
   def new
     @user=User.new
   end
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
-      redirect_to  user_path(@user.id)
+      session[:user_id]=@user.id
+      redirect_to user_path(@user.id)
     else
       render :new
     end
